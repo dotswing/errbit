2f5ed0e1d7ae6da8357474b6f6353aff30c9d304
pathdotswing <pianjung@hotmail.com>
2016-05-05 19:40:32 +0700

    change config

--- config/puma.default.rb
+++ config/puma.default.rb
@@ -1,14 +1,15 @@
 # https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+workers 1#Integer(ENV['WEB_CONCURRENCY'] || 2)
+# threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads 1, 10 #threads_count, threads_count
 
 preload_app!
 
-rackup DefaultRackup
-port ENV['PORT'] || 8080
-environment ENV['RACK_ENV'] || 'development'
+bind 'unix:///tmp/errbit/errbit-puma.sock'
+# rackup DefaultRackup
+# port ENV['PORT'] || 8080
+# environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
